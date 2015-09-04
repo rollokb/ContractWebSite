bfae7ffaeaf4f734b1528081f8e0d45f4e3620bb
Rollo <rollokb@gmail.com>
2015-09-04 20:05:59 +0100

    Basscss

--- gulpfile.js/config/sass.js
+++ gulpfile.js/config/sass.js
@@ -5,7 +5,9 @@ module.exports = {
   src: config.sourceAssets + "/stylesheets/**/*.{sass,scss}",
   dest: config.publicAssets + '/stylesheets',
   settings: {
-    indentedSyntax: true, // Enable .sass syntax!
-    imagePath: 'images' // Used by the image-url helper
+    imagePath: 'images', // Used by the image-url helper
+    includePaths: [
+      './bower_components/basscss-sass',
+    ],
   }
 }
